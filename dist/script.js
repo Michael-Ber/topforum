0d70219139d93caae309b870c55b80e66e570eaf
Michael <example@gmail.com>
2021-05-04 20:33:05 +0800

    landing done, customer slider done

--- dist/script.js
+++ dist/script.js
@@ -93,7 +93,30 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-console.log('hello');
+window.addEventListener("DOMContentLoaded", () => {
+  const swiper = new Swiper('.customer__frame', {
+    // Optional parameters
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.customer__arrow-right',
+      prevEl: '.customer__arrow-left'
+    },
+    spaceBetween: 50,
+    speed: 600,
+    grabCursor: true,
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        slidesPerGroup: 1
+      },
+      992: {
+        slidesPerView: 2,
+        slidesPerGroup: 2
+      }
+    }
+  });
+});
 
 /***/ })
 
