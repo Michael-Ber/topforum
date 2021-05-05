ca3b5a4bde7f86f63fd27fe73be3c6d16c6d8a18
Michael <example@gmail.com>
2021-05-05 14:31:14 +0800

    sliders on main page done

--- dist/script.js
+++ dist/script.js
@@ -94,7 +94,7 @@
 /***/ (function(module, exports) {
 
 window.addEventListener("DOMContentLoaded", () => {
-  const swiper = new Swiper('.customer__frame', {
+  const customerSwiper = new Swiper('.customer__frame', {
     // Optional parameters
     loop: true,
     // Navigation arrows
@@ -113,6 +113,40 @@ window.addEventListener("DOMContentLoaded", () => {
       992: {
         slidesPerView: 2,
         slidesPerGroup: 2
+      }
+    }
+  });
+  const clientsSwiper = new Swiper('.clients__carousel-frame', {
+    // Optional parameters
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.clients__arrow-right',
+      prevEl: '.clients__arrow-left'
+    },
+    spaceBetween: 13,
+    speed: 600,
+    grabCursor: true,
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        slidesPerGroup: 1
+      },
+      410: {
+        slidesPerView: 2,
+        slidesPerGroup: 2
+      },
+      576: {
+        slidesPerView: 3,
+        slidesPerGroup: 3
+      },
+      768: {
+        slidesPerView: 5,
+        slidesPerGroup: 5
+      },
+      992: {
+        slidesPerView: 6,
+        slidesPerGroup: 6
       }
     }
   });
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,5 +1,5 @@
 window.addEventListener("DOMContentLoaded", () => {
-    const swiper = new Swiper('.customer__frame', {
+    const customerSwiper = new Swiper('.customer__frame', {
         // Optional parameters
         loop: true,
       
@@ -21,5 +21,41 @@ window.addEventListener("DOMContentLoaded", () => {
                 slidesPerGroup: 2
             }
         }
-      });
+    });
+    const clientsSwiper = new Swiper('.clients__carousel-frame', {
+        // Optional parameters
+        loop: true,
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.clients__arrow-right',
+          prevEl: '.clients__arrow-left',
+        },
+        spaceBetween: 13,
+        speed: 600,
+        grabCursor: true,
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+                slidesPerGroup: 1,
+            },
+            410: {
+                slidesPerView: 2,
+                slidesPerGroup: 2,
+            },
+            576: {
+                slidesPerView: 3,
+                slidesPerGroup: 3
+            },
+            768: {
+                slidesPerView: 5,
+                slidesPerGroup: 5
+            },
+            992: {
+                slidesPerView: 6,
+                slidesPerGroup: 6
+            }
+        }
+    });
+
 });
